213c515ad51762d05e19c8049484ee9114e34248
“Saima <rahman.saima92@gmail.com>
2020-01-20 18:52:45 -0500

    done

--- app/models/plant.rb
+++ app/models/plant.rb
@@ -3,4 +3,7 @@ class Plant < ApplicationRecord
   validates :name, presence: true
   validates :image, presence: true
   validates :name, uniqueness: true
+  validates :description, presence: true
+  validates :care, presence: true
+  validates :image, presence: true
 end
